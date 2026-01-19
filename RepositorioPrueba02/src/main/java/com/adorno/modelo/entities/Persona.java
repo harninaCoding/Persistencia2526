d4768ed5b0bc10c352f560f4c7703dee5aa4019a
programacion WEB <jasanchezm01@iesarroyoharnina.es>
2026-01-19 10:55:54 +0100

    repos primeros pasos

--- RepositorioPrueba02/src/main/java/com/adorno/modelo/entities/Persona.java
+++ RepositorioPrueba02/src/main/java/com/adorno/modelo/entities/Persona.java
@@ -1,22 +1,38 @@
 package com.adorno.modelo.entities;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
 
+//Solo hace el nonargsConstructor y RequiredArgsConstructor
+@Data
 @Entity
-public class Persona {
+public class Persona implements Serializable{
+	//Cada cambio en Persona crea una forma de serializacion diferente. Por eso es importante
+	//estaclecer la version. Si hacemos un cambio en un campo de la clase deberiamos cambiar el serialVersion
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NonNull
 	private String nombre;
+	@NonNull
 	private String apellidos;
 	//En este caso Persona es la propietaria de la relacion. porque alberga el elemento de la relacion
 	@OneToOne
+	@NonNull
 	private Direccion direccion;
 	@Embedded
+	@NonNull
 	private Poliza poliza;
+
+	
 }
--- RepositorioPrueba02/src/test/java/com/adorno/modelo/repos/PersonaRepositoryTest.java
+++ RepositorioPrueba02/src/test/java/com/adorno/modelo/repos/PersonaRepositoryTest.java
@@ -1,19 +1,34 @@
 package com.adorno.modelo.repos;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.adorno.modelo.entities.Direccion;
+import com.adorno.modelo.entities.Persona;
+import com.adorno.modelo.entities.Poliza;
+
+@SpringBootTest
 class PersonaRepositoryTest {
-	
-	//Para decirle a Spring que me inyecte la instancia de PersonaRepository
+
+	// Para decirle a Spring que me inyecte la instancia de PersonaRepository
 	@Autowired
 	PersonaRepository personaRepository;
+	@Autowired
+	DireccionRepository direccionRepository;
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Direccion direccion = new Direccion("sol", 1);
+		Poliza poliza = new Poliza(1l, LocalDate.now());
+		//primer error intentar grabar un objeto que no existe en la bbdd
+		//en este caso direccion
+		//direccion no tiene id hasta que no se graba en la bbdd (persiste) y ese id es necesario para 
+		//que funcione como clave externa en Persona
+		direccion = direccionRepository.save(direccion);
+		Persona persona = new Persona("luis", "panfleto", direccion, poliza);
+		personaRepository.save(persona);
 	}
-
 }
